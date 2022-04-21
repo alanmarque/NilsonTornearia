e0e290427b8a2951ad2c66d86f2a758183098821
Your Name <you@example.com>
2022-04-20 22:32:14 -0300

    .

--- app/src/main/java/com/example/nilsontornearia/SecondActivity.kt
+++ app/src/main/java/com/example/nilsontornearia/SecondActivity.kt
@@ -1,11 +1,27 @@
 package com.example.nilsontornearia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
     }
+
+
+    private fun openNextActivity(){
+        val intent = Intent(this, NewActivity::class.java)
+        startActivity(intent)
+
+
+    }
+
+    fun onCheckboxClicked(view: View){
+
+    }
+
+
 }

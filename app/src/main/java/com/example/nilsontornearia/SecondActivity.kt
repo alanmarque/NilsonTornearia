647d44287a6ceb8fee33e703419e72cac13c9e9d
Your Name <you@example.com>
2022-05-04 20:07:26 -0300

    .

--- app/src/main/java/com/example/nilsontornearia/SecondActivity.kt
+++ app/src/main/java/com/example/nilsontornearia/SecondActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_second.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,19 @@ class SecondActivity : AppCompatActivity() {
         }
     }
 
+    private fun configuraMenuLateral() {
+        // ícone de menu (hamburger) para mostrar o menu
+        var toogle = ActionBarDrawerToggle(this,
+            layoutMenuLateral,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close)
+
+        layoutMenuLateral.addDrawerListener(toogle)
+        toogle.syncState()
+
+        menu_lateral.setNavigationItemSelectedListener(this)
+    }
 
     private fun openNextActivity(){
         val intent = Intent(this, MainActivity::class.java)

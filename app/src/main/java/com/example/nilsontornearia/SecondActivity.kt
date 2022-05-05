ec9b6f7210a1b9ba770cf2392089c837f2cd9fc0
Your Name <you@example.com>
2022-05-04 21:57:26 -0300

    feat: add click on hamburguer menu

--- app/src/main/java/com/example/nilsontornearia/SecondActivity.kt
+++ app/src/main/java/com/example/nilsontornearia/SecondActivity.kt
@@ -1,30 +1,37 @@
 package com.example.nilsontornearia
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_second.*
 import kotlinx.android.synthetic.main.toolbar.*
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val context: Context get() = this
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         btnVoltar.setOnClickListener{
             openNextActivity()
         }
+
+        // configuração do menu lateral
+        configuraMenuLateral()
     }
 
     private fun configuraMenuLateral() {
         // ícone de menu (hamburger) para mostrar o menu
-        var toogle = ActionBarDrawerToggle(this,
-            layoutMenuLateral,
-            toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close)
+        var toogle = ActionBarDrawerToggle(this, layoutMenuLateral, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
 
         layoutMenuLateral.addDrawerListener(toogle)
         toogle.syncState()
@@ -32,14 +39,36 @@ class SecondActivity : AppCompatActivity() {
         menu_lateral.setNavigationItemSelectedListener(this)
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_diciplinas -> {
+                Toast.makeText(this, "Clicou Disciplinas", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.nav_mensagens -> {
+                Toast.makeText(this, "Clicou Mensagens", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.nav_forum -> {
+                Toast.makeText(this, "Clicou Forum", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.nav_localizacao -> {
+                Toast.makeText(this, "Clicou Localização", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.nav_config -> {
+                Toast.makeText(this, "Clicou Config", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // fecha menu depois de tratar o evento
+        layoutMenuLateral.closeDrawer(GravityCompat.START)
+        return true
+    }
+
     private fun openNextActivity(){
         val intent = Intent(this, MainActivity::class.java)
         super.onBackPressed()
     }
-
-    fun onCheckboxClicked(view: View){
-
-    }
-
-
 }

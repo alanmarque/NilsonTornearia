b39b07dffa8592af757a690b0eba922b627913d7
alanmarque <alan.marque0308@gmail.com>
2022-06-01 19:45:15 -0300

    .

--- app/src/main/java/com/example/nilsontornearia/activity_pedido.kt
+++ app/src/main/java/com/example/nilsontornearia/activity_pedido.kt
@@ -1,5 +1,6 @@
 package com.example.nilsontornearia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_pedido.*
@@ -9,9 +10,15 @@ class activity_pedido : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pedido)
-        btnFinPedido.setOnClickListener{
-            onBackPressed()
+        btnFinPedido.setOnClickListener {
+            openMainActivity()
+        }
+    }
+
+        private fun openMainActivity(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
+
     }
-}

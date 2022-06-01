d1c09971a8ae937bba40c87f8a1e7eeed11ff886
alanmarque <alan.marque0308@gmail.com>
2022-06-01 16:09:00 -0300

    feat: adicionando a função voltar ao botão finalizar pedido

--- app/src/main/java/com/example/nilsontornearia/activity_pedido.kt
+++ app/src/main/java/com/example/nilsontornearia/activity_pedido.kt
@@ -2,10 +2,16 @@ package com.example.nilsontornearia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_pedido.*
+import kotlinx.android.synthetic.main.activity_second.*
 
 class activity_pedido : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pedido)
+        btnFinPedido.setOnClickListener{
+            onBackPressed()
+        }
+
     }
 }
